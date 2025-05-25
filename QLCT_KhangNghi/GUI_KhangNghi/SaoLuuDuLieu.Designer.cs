2e5a72c650b79ed2c73b3b3e8dab047f57ee1597
Dat1030 <datkienluu@gmail.com>
2025-05-25 09:46:46 +0700

    them giao dien sao luu

---
+++ QLCT_KhangNghi/GUI_KhangNghi/SaoLuuDuLieu.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace GUI_KhangNghi
+{
+    partial class SaoLuuDuLieu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnThucHienPhucHoi = new Guna.UI2.WinForms.Guna2Button();
+            this.txtDuongDanPhucHoi = new Guna.UI2.WinForms.Guna2TextBox();
+            this.btnChonThuMucPhucHoi = new Guna.UI2.WinForms.Guna2Button();
+            this.txtDuongDan = new Guna.UI2.WinForms.Guna2TextBox();
+            this.btnThucHienSaoLuu = new Guna.UI2.WinForms.Guna2Button();
+            this.btnChonThuMuc = new Guna.UI2.WinForms.Guna2Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.guna2Panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2Panel1.Controls.Add(this.label4);
+            this.guna2Panel1.Controls.Add(this.label5);
+            this.guna2Panel1.Controls.Add(this.label2);
+            this.guna2Panel1.Controls.Add(this.label1);
+            this.guna2Panel1.Controls.Add(this.btnThucHienPhucHoi);
+            this.guna2Panel1.Controls.Add(this.txtDuongDanPhucHoi);
+            this.guna2Panel1.Controls.Add(this.btnChonThuMucPhucHoi);
+            this.guna2Panel1.Controls.Add(this.txtDuongDan);
+            this.guna2Panel1.Controls.Add(this.btnThucHienSaoLuu);
+            this.guna2Panel1.Controls.Add(this.btnChonThuMuc);
+            this.guna2Panel1.Location = new System.Drawing.Point(-21, 140);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(1089, 430);
+            this.guna2Panel1.TabIndex = 74;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.Location = new System.Drawing.Point(23, 316);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(243, 29);
+            this.label4.TabIndex = 74;
+            this.label4.Text = "Đường dẫn thư mục:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label5.Location = new System.Drawing.Point(23, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(242, 32);
+            this.label5.TabIndex = 73;
+            this.label5.Text = "Phục hồi dữ liệu:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(22, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(243, 29);
+            this.label2.TabIndex = 72;
+            this.label2.Text = "Đường dẫn thư mục:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(22, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 32);
+            this.label1.TabIndex = 71;
+            this.label1.Text = "Sao lưu dữ liệu:";
+            // 
+            // btnThucHienPhucHoi
+            // 
+            this.btnThucHienPhucHoi.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnThucHienPhucHoi.BorderRadius = 10;
+            this.btnThucHienPhucHoi.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnThucHienPhucHoi.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnThucHienPhucHoi.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnThucHienPhucHoi.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnThucHienPhucHoi.FillColor = System.Drawing.Color.RoyalBlue;
+            this.btnThucHienPhucHoi.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnThucHienPhucHoi.ForeColor = System.Drawing.Color.White;
+            this.btnThucHienPhucHoi.Location = new System.Drawing.Point(830, 349);
+            this.btnThucHienPhucHoi.Name = "btnThucHienPhucHoi";
+            this.btnThucHienPhucHoi.Size = new System.Drawing.Size(201, 55);
+            this.btnThucHienPhucHoi.TabIndex = 70;
+            this.btnThucHienPhucHoi.Text = "Thực hiện phục hồi";
+            this.btnThucHienPhucHoi.Click += new System.EventHandler(this.btnThucHienPhucHoi_Click);
+            // 
+            // txtDuongDanPhucHoi
+            // 
+            this.txtDuongDanPhucHoi.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDuongDanPhucHoi.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtDuongDanPhucHoi.DefaultText = "";
+            this.txtDuongDanPhucHoi.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtDuongDanPhucHoi.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtDuongDanPhucHoi.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtDuongDanPhucHoi.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtDuongDanPhucHoi.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtDuongDanPhucHoi.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtDuongDanPhucHoi.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtDuongDanPhucHoi.Location = new System.Drawing.Point(27, 349);
+            this.txtDuongDanPhucHoi.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDuongDanPhucHoi.Name = "txtDuongDanPhucHoi";
+            this.txtDuongDanPhucHoi.PlaceholderText = "";
+            this.txtDuongDanPhucHoi.SelectedText = "";
+            this.txtDuongDanPhucHoi.Size = new System.Drawing.Size(581, 55);
+            this.txtDuongDanPhucHoi.TabIndex = 66;
+            // 
+            // btnChonThuMucPhucHoi
+            // 
+            this.btnChonThuMucPhucHoi.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChonThuMucPhucHoi.BorderRadius = 10;
+            this.btnChonThuMucPhucHoi.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnChonThuMucPhucHoi.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnChonThuMucPhucHoi.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnChonThuMucPhucHoi.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnChonThuMucPhucHoi.FillColor = System.Drawing.Color.RoyalBlue;
+            this.btnChonThuMucPhucHoi.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnChonThuMucPhucHoi.ForeColor = System.Drawing.Color.White;
+            this.btnChonThuMucPhucHoi.Location = new System.Drawing.Point(639, 349);
+            this.btnChonThuMucPhucHoi.Name = "btnChonThuMucPhucHoi";
+            this.btnChonThuMucPhucHoi.Size = new System.Drawing.Size(156, 55);
+            this.btnChonThuMucPhucHoi.TabIndex = 68;
+            this.btnChonThuMucPhucHoi.Text = "Chọn thư mục";
+            this.btnChonThuMucPhucHoi.Click += new System.EventHandler(this.btnChonThuMucPhucHoi_Click);
+            // 
+            // txtDuongDan
+            // 
+            this.txtDuongDan.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDuongDan.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtDuongDan.DefaultText = "";
+            this.txtDuongDan.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtDuongDan.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtDuongDan.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtDuongDan.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtDuongDan.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtDuongDan.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtDuongDan.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtDuongDan.Location = new System.Drawing.Point(27, 129);
+            this.txtDuongDan.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDuongDan.Name = "txtDuongDan";
+            this.txtDuongDan.PlaceholderText = "";
+            this.txtDuongDan.SelectedText = "";
+            this.txtDuongDan.Size = new System.Drawing.Size(581, 55);
+            this.txtDuongDan.TabIndex = 0;
+            // 
+            // btnThucHienSaoLuu
+            // 
+            this.btnThucHienSaoLuu.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnThucHienSaoLuu.BorderRadius = 10;
+            this.btnThucHienSaoLuu.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnThucHienSaoLuu.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnThucHienSaoLuu.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnThucHienSaoLuu.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnThucHienSaoLuu.FillColor = System.Drawing.Color.RoyalBlue;
+            this.btnThucHienSaoLuu.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnThucHienSaoLuu.ForeColor = System.Drawing.Color.White;
+            this.btnThucHienSaoLuu.Location = new System.Drawing.Point(830, 129);
+            this.btnThucHienSaoLuu.Name = "btnThucHienSaoLuu";
+            this.btnThucHienSaoLuu.Size = new System.Drawing.Size(201, 55);
+            this.btnThucHienSaoLuu.TabIndex = 12;
+            this.btnThucHienSaoLuu.Text = "Thực hiện sao lưu";
+            this.btnThucHienSaoLuu.Click += new System.EventHandler(this.btnThucHienSaoLuu_Click);
+            // 
+            // btnChonThuMuc
+            // 
+            this.btnChonThuMuc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChonThuMuc.BorderRadius = 10;
+            this.btnChonThuMuc.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnChonThuMuc.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnChonThuMuc.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnChonThuMuc.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnChonThuMuc.FillColor = System.Drawing.Color.RoyalBlue;
+            this.btnChonThuMuc.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnChonThuMuc.ForeColor = System.Drawing.Color.White;
+            this.btnChonThuMuc.Location = new System.Drawing.Point(639, 129);
+            this.btnChonThuMuc.Name = "btnChonThuMuc";
+            this.btnChonThuMuc.Size = new System.Drawing.Size(156, 55);
+            this.btnChonThuMuc.TabIndex = 9;
+            this.btnChonThuMuc.Text = "Chọn thư mục";
+            this.btnChonThuMuc.Click += new System.EventHandler(this.btnChonThuMuc_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(294, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(535, 39);
+            this.label3.TabIndex = 75;
+            this.label3.Text = "SAO LƯU VÀ PHỤC HỒI DỮ LIỆU";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // SaoLuuDuLieu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1047, 602);
+            this.Controls.Add(this.guna2Panel1);
+            this.Controls.Add(this.label3);
+            this.Name = "SaoLuuDuLieu";
+            this.Text = "SaoLuuDuLieu";
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Button btnThucHienPhucHoi;
+        private Guna.UI2.WinForms.Guna2TextBox txtDuongDanPhucHoi;
+        private Guna.UI2.WinForms.Guna2Button btnChonThuMucPhucHoi;
+        private Guna.UI2.WinForms.Guna2TextBox txtDuongDan;
+        private Guna.UI2.WinForms.Guna2Button btnThucHienSaoLuu;
+        private Guna.UI2.WinForms.Guna2Button btnChonThuMuc;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ QLCT_KhangNghi/GUI_KhangNghi/SaoLuuDuLieu.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BUS_KhangNghi;
+namespace GUI_KhangNghi
+{
+    public partial class SaoLuuDuLieu : Form
+    {
+        private DatabaseUtilityBUS busDatabaseUtil;
+        public SaoLuuDuLieu()
+        {
+            InitializeComponent();
+            busDatabaseUtil = new DatabaseUtilityBUS();
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void btnChonThuMuc_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                folderDialog.Description = "Chọn thư mục để lưu file sao lưu";
+                if (!string.IsNullOrWhiteSpace(txtDuongDan.Text) && Directory.Exists(txtDuongDan.Text))
+                {
+                    folderDialog.SelectedPath = txtDuongDan.Text;
+                }
+                else
+                {
+                    folderDialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                }
+                folderDialog.ShowNewFolderButton = true;
+
+                if (folderDialog.ShowDialog(this) == DialogResult.OK)
+                {
+                    txtDuongDan.Text = folderDialog.SelectedPath;
+                }
+            }
+        }
+
+        private void btnThucHienSaoLuu_Click(object sender, EventArgs e)
+        {
+            string selectedFolderPath = txtDuongDan.Text.Trim();
+
+            if (string.IsNullOrEmpty(selectedFolderPath))
+            {
+                MessageBox.Show("Vui lòng chọn một thư mục để lưu file sao lưu.", "Thiếu thông tin", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                btnChonThuMuc.Focus();
+                return;
+            }
+
+            if (!Directory.Exists(selectedFolderPath))
+            {
+                MessageBox.Show($"Thư mục '{selectedFolderPath}' không tồn tại. Vui lòng chọn một thư mục hợp lệ.", "Thư mục không hợp lệ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                btnChonThuMuc.Focus();
+                return;
+            }
+
+            string databaseName = "KhangNghiDB";
+            string fileName = $"{databaseName}_Full_{DateTime.Now:yyyyMMdd_HHmmss}.bak";
+            string fullBackupFilePath = Path.Combine(selectedFolderPath, fileName);
+
+            DialogResult confirmBackup = MessageBox.Show(
+                $"Bạn có chắc chắn muốn sao lưu CSDL {databaseName} vào:\n{fullBackupFilePath}\n\n" +
+                "LƯU Ý: Nếu file sao lưu đã tồn tại, nó sẽ được ghi đè.",
+                "Xác nhận Sao Lưu",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (confirmBackup == DialogResult.Yes)
+            {
+                try
+                {
+                    string ketQua = busDatabaseUtil.PerformBackupOperation(fullBackupFilePath);
+
+                    if (ketQua.StartsWith("Sao lưu CSDL") && ketQua.Contains("thành công"))
+                    {
+                        MessageBox.Show(ketQua, "Sao Lưu Hoàn Tất", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show(ketQua, "Thông Báo Sao Lưu", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Đã xảy ra lỗi không mong muốn trong quá trình sao lưu:\n\n{ex.Message}",
+                                    "Lỗi Nghiêm Trọng",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnChonThuMucPhucHoi_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "C:\\";
+                openFileDialog.Filter = "Backup Files (*.bak)|*.bak|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+                openFileDialog.Title = "Chọn file sao lưu để phục hồi";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtDuongDanPhucHoi.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private void btnThucHienPhucHoi_Click(object sender, EventArgs e)
+        {
+            string backupFilePath = txtDuongDanPhucHoi.Text;
+
+            if (string.IsNullOrWhiteSpace(backupFilePath))
+            {
+                MessageBox.Show("Vui lòng chọn một file sao lưu (.bak) để phục hồi.", "Chưa chọn file", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!File.Exists(backupFilePath))
+            {
+                MessageBox.Show($"File sao lưu không tồn tại tại đường dẫn đã chọn:\n{backupFilePath}", "File không tồn tại", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            DialogResult confirmation = MessageBox.Show(
+                $"Bạn có chắc chắn muốn phục hồi cơ sở dữ liệu từ file:\n{backupFilePath}\n\n" +
+                "THAO TÁC NÀY SẼ GHI ĐÈ LÊN CƠ SỞ DỮ LIỆU HIỆN TẠI VÀ KHÔNG THỂ HOÀN TÁC!\n" +
+                "Hãy đảm bảo bạn đã sao lưu dữ liệu quan trọng (nếu cần) trước khi tiếp tục.",
+                "Xác nhận Phục Hồi CSDL",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning,
+                MessageBoxDefaultButton.Button2);
+
+            if (confirmation == DialogResult.Yes)
+            {
+                string resultMessage = string.Empty;
+                resultMessage = busDatabaseUtil.PerformRestoreOperation(backupFilePath);
+                if (resultMessage.ToLower().Contains("thành công"))
+                {
+                    MessageBox.Show(resultMessage, "Phục hồi thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show(resultMessage, "Phục hồi thất bại", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
